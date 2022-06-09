a42d0d9e9e9a16743fd520b1ef43ac9d5bf11d6c
Bojan <bojanforgaming@hotmail.com>
2022-06-09 10:15:08 +0200

    styled buttons and fixed trpc url gen

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { getOptionsForVote } from '../utils/getRandomPokemon';
 import { trpc } from '../utils/trpc';
 
+const btn =
+  'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
+
 const Home: NextPage = () => {
   const [ids, updateIds] = useState(getOptionsForVote());
 
@@ -13,6 +16,12 @@ const Home: NextPage = () => {
   const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }]);
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
+  const voteForRoundest = (selected: number) => {
+    // todo: Fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  };
 
   return (
     <div className=''>
@@ -38,6 +47,9 @@ const Home: NextPage = () => {
             <div className='capitalize text-xl text-center mt-[-2rem]'>
               {firstPokemon.data?.name}
             </div>
+            <button className={btn} onClick={() => voteForRoundest(first)}>
+              Rounder
+            </button>
           </div>
           <div className='p-8'>Vs</div>
           <div className='w-64 h-64 flex flex-col items-center'>
